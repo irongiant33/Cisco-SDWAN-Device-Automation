a6843f01b6764cd047df6813933fa6e24af67365
smiller2 <samuel.j.miller97.mil@army.mil>
2026-06-23 16:10:37 -0400

    adding keyboard interrupt handling

--- main.py
+++ main.py
@@ -451,4 +451,8 @@ def main():
     SDWANShell(session, base_url).cmdloop()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n👋 Terminating operations processes. Goodbye.")
+        sys.exit(0)
